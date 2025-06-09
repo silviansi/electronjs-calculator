f0684d633ecf141ae078324ebe298b18cf52dc4e
silviansi <silviansi24@gmail.com>
2025-06-09 20:56:47 +0700

    feat: add main process script to initialize electron app window and settings

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 350,
+        height: 620,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
