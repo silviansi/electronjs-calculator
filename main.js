2e57909dce6212b1dc47610e31c217b830b93b68
silviansi <silviansi24@gmail.com>
2025-06-10 10:25:17 +0700

    feat: add icon calculator

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function createWindow () {
     const win = new BrowserWindow({
         width: 350,
         height: 620,
+        icon: path.join(__dirname, 'icon.png'),
         resizable: false,
         webPreferences: {
             nodeIntegration: true,
